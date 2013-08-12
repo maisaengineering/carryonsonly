e3899aa03fd2d8dc9ddf0fb95683083b8c99d304
maisaengineering <contact@maisasolutions.com>
2013-08-12 16:43:53 +0530

    first commit

---
+++ app/models/user.rb
@@ -0,0 +1,7 @@
+class User < ActiveRecord::Base
+  attr_accessible :email, :zipcode
+  
+  validates_format_of :email, :with => /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\Z/i, :message => "Invaild E-mail Format"
+  validates :zipcode, :numericality => {:message => "Enter only digits"} 
+  validates :email, :uniqueness => true
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+WeekenderApp::Application.config.secret_token = '0198b451abc01e3b0a35d3616ae7b46863459089bb14cf09e07a3cac0ffbda8a256ffacfa909d1cfd451fdb4bc34ab87b974bb418687b847d26fd4771171e2a0'
